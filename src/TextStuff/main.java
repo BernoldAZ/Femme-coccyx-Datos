ff36c8df01479c5aa9003484449adb011e215c6e
Bernold <bernoldaz@gmail.com>
2018-11-23 12:38:49 -0600

    needed

--- src/TextStuff/main.java
+++ src/TextStuff/main.java
@@ -5,7 +5,7 @@ public class main {
 
 	public static void main(String[] args) {
 		TextAnalyzer text = new TextAnalyzer("C:\\Users\\"+"Esteban Madrigal"+"\\Documents\\GitHub\\"+"Femme-coccyx"+"\\src\\text1.txt");
-		Azure azure = new Azure("https://cdn.fstoppers.com/styles/large-16-9/s3/lead/2018/06/ultra-wide-mistakes-lead-image.jpg");
+		//Azure azure = new Azure("https://cdn.fstoppers.com/styles/large-16-9/s3/lead/2018/06/ultra-wide-mistakes-lead-image.jpg");
 
 	}
 
